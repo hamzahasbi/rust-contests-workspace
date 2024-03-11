7439729696c18dc5961975063ed9aaeb0b8f993e
Hamza Hasbi <hamza.hasbi@gmail.com>
2024-03-11 23:04:04 +0000

    Add: first test using Egor rust library

---
+++ a_word_capitalization/src/main.rs
@@ -0,0 +1,52 @@
+//{"name":"A. Word Capitalization","group":"Codeforces - Codeforces Round 172 (Div. 2)","url":"https://codeforces.com/problemset/problem/281/A","interactive":false,"timeLimit":2000,"tests":[{"input":"ApPLe\n","output":"ApPLe\n"},{"input":"konjac\n","output":"Konjac\n"}],"testType":"single","input":{"type":"stdin","fileName":null,"pattern":null},"output":{"type":"stdout","fileName":null,"pattern":null},"languages":{"java":{"taskClass":"AWordCapitalization"}}}
+
+use algo_lib::io::input::Input;
+use algo_lib::io::output::Output;
+
+type PreCalc = ();
+
+fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &PreCalc) {
+    let mut s: Vec<char> = input.read_line().chars().collect();
+    s[0] = s[0].to_ascii_uppercase();
+    out.print_line(s.iter().collect::<String>());
+}
+
+pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
+    let pre_calc = ();
+
+    #[allow(dead_code)]
+    enum TestType {
+        Single,
+        MultiNumber,
+        MultiEof,
+    }
+    let test_type = TestType::Single;
+    match test_type {
+        TestType::Single => solve(&mut input, &mut output, 1, &pre_calc),
+        TestType::MultiNumber => {
+            let t = input.read();
+            for i in 0usize..t {
+                solve(&mut input, &mut output, i + 1, &pre_calc);
+            }
+        }
+        TestType::MultiEof => {
+            let mut i = 1;
+            while input.peek().is_some() {
+                solve(&mut input, &mut output, i, &pre_calc);
+                i += 1;
+            }
+        }
+    }
+    output.flush();
+    input.skip_whitespace();
+    input.peek().is_none()
+}
+
+
+//START MAIN
+mod tester;
+
+fn main() {
+    tester::run_tests();
+}
+//END MAIN
